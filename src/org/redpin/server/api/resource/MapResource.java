3970be4d87d71473e2ac703fbc2d68ca6c3c549d
unknown <waqas.siddiqui@Karachi-520.360training.com>
2014-06-10 19:29:26 +0500

    imported redpin standalone to web server

--- src/org/redpin/server/api/resource/MapResource.java
+++ src/org/redpin/server/api/resource/MapResource.java
@@ -3,43 +3,43 @@ package org.redpin.server.api.resource;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Request;
-import javax.ws.rs.core.UriInfo;
 
-import org.redpin.server.core.Map;
-
+import org.redpin.server.standalone.core.Map;
+import org.redpin.server.standalone.json.GsonFactory;
 
 @Path("/map")
 public class MapResource {
-	
-	@Context
-    UriInfo uriInfo;
-	
-	@Context
-    Request request;
-	
-	/*@GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String respondAsReady() {
-        return "Demo service is ready!";
-    }*/
-	
 	@GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Map> getSamplePerson() {
-		Map m = new Map();
-		 m.setMapName("Home");
-		 m.setMapURL("http://192.4.70.1/maps/map1.png");
-		
-		List<Map> l = new ArrayList<Map>();
-		
-		l.add(m);
-		
-		return l;
+    public List<Map> getAll() {
+         List<Map> listAll = new ArrayList<Map>();
+         
+         Map m = new Map();
+         m.setId(1);
+         m.setMapName("Home");
+         m.setMapURL("http://10.12.100.9/mapuploads/home.png");
+                 
+         listAll.add(m);
+         
+         return listAll;
     }
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Map postMap(String jsonRequest) {
+		
+		System.out.println(jsonRequest);
+		
+		Map map = GsonFactory.getGsonInstance().fromJson(jsonRequest, Map.class);
+		//map = mapHome.add(map);
+		
+		return map;
+	}
 }
